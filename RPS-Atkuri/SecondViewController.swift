3b95374a29876d0fd3b70835f9c0e6d6ea59c6c6
student <s530458@ol10019m.nwmissouri.edu>
2018-02-19 17:08:27 -0600

    Initial Commit

---
+++ RPS-Atkuri/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RPS-Atkuri
+//
+//  Created by Atkuri,Ashok on 2/19/18.
+//  Copyright © 2018 Atkuri,Ashok. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
